8cb8d8b5991e8223dc096f643d54d002b9ccc289
hoang134 <hoangha13499.kshb@gmail.com>
2021-03-16 10:10:49 +0700

    fix messenger

--- app/Http/Controllers/Admin/MessengerController.php
+++ app/Http/Controllers/Admin/MessengerController.php
@@ -17,12 +17,12 @@ class MessengerController extends Controller
     public function index()
     {
 
-       //$messengers = DB::table('messengers')->where('user_to',Auth::user()->tendangnhap)->orderBy('created_at','desc')->get();
-       $listUserTos = DB::table('messengers')->select('user_from')->where('user_from','!=',Auth::user()->tendangnhap)
+        //$messengers = DB::table('messengers')->where('user_to',Auth::user()->tendangnhap)->orderBy('created_at','desc')->get();
+        $listUserTos = DB::table('messengers')->select('user_from')->where('user_from','!=',Auth::user()->tendangnhap)
             ->groupBy('user_from')->get();
-      return view('admin.messengers.index',[
-          'listUserTos'=>$listUserTos
-      ]);
+        return view('admin.messengers.index',[
+            'listUserTos'=>$listUserTos
+        ]);
     }
 
     public function detail( Request $request)
@@ -30,7 +30,6 @@ class MessengerController extends Controller
 
         $messengers = DB::table('messengers')->where('user_from',Auth::user()->tendangnhap)->where('user_to',$request->tendangnhap)
             ->orWhere('user_to',Auth::user()->tendangnhap)->where('user_from',$request->tendangnhap)->get();
-
         return view('admin.messengers.detail-messenger',[
             'messengers'=> $messengers,
             'user_from'=> $request->tendangnhap,
@@ -39,26 +38,35 @@ class MessengerController extends Controller
 
     public function reply(Request $request)
     {
-      $messenger = new Messenger();
-      $messenger ->user_from = Auth::user()->tendangnhap;
-      $messenger ->user_to = $request->tendangnhap;
-      $messenger ->content = $request->messenger;
-      $messenger ->belong = Messenger::BELONG_ADMIN;
-      $messenger->save();
-      event(
-          $e = new ChatEvent($messenger)
-      );
+        $messenger = new Messenger();
+        $messenger ->user_from = Auth::user()->tendangnhap;
+        $messenger ->user_to = $request->tendangnhap;
+        $messenger ->content = $request->messenger;
+        $messenger ->belong = Messenger::BELONG_ADMIN;
+        $messenger->save();
+        event(
+            $e = new ChatEvent($messenger)
+        );
+//        echo '<div class="d-flex justify-content-end mb-4">
+//                <div class="msg_cotainer_send">' .
+//                  $messenger->content .
+//                  '<!-- <span class="msg_time_send">{{$messenger->created_at}}</span> -->
+//                </div>
+//                <div class="img_cont_msg">
+//              <img src="' . asset('/images/1.png') . '" class="rounded-circle user_img_msg">
+//                </div>
+//              </div>';
 
-      echo '<div class="media w-75 ml-auto mb-3">
-              <div class="media-body">
-                <div class="bg-primary rounded py-2 px-3 mb-2">
-                  <p class="text-small mb-0 text-white">' .
-                $messenger->content .
-                '</p>
-                </div>
-                <p class="small text-muted">'.$messenger->created_at .'</p>
-              </div>
-            </div>';
+
+        echo  ' <div class="media w-75 ml-auto mb-3" >
+                                <div class="media-body">
+                                  <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">'.$messenger->content.'</p>
+                                  </div>
+                                 <p class="small text-muted">'.$messenger->created_at.'</p>
+                                </div>
+                              </div>';
+
     }
 
 
